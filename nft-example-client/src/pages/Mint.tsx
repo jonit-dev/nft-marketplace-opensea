9656f307aa606c7903ab2ed7a748ca4185f2faa2
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-03-31 14:35:15 -0700

    mint page, dependency fixes

--- nft-example-client/src/pages/Mint.tsx
+++ nft-example-client/src/pages/Mint.tsx
@@ -1,12 +1,55 @@
 import React from "react";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+import { InternalPage } from "../components/InternalPage";
 import { Page } from "../components/Page";
+import { useForm } from "../hooks/useForm";
 
 interface IProps {}
 
 export const Mint: React.FC<IProps> = (props) => {
+  const [formValues, setFormValues] = useForm({
+    tokenId: 0,
+    amount: 0,
+    contractAddress: "",
+  });
+
   return (
     <Page>
-      <h1 className="title">Mint</h1>
+      <InternalPage>
+        <h1 className="title">Mint</h1>
+
+        <Input
+          label="Token ID"
+          value={formValues.tokenId}
+          onChange={(e) => {
+            setFormValues("tokenId", e.target.value);
+          }}
+          type="number"
+          placeholder="Token ID"
+        />
+
+        <Input
+          label="Amount"
+          value={formValues.amount}
+          onChange={(e) => {
+            setFormValues("amount", e.target.value);
+          }}
+          type="number"
+          placeholder="Amount"
+        />
+        <Input
+          label="Contract Address"
+          value={formValues.contractAddress}
+          onChange={(e) => {
+            setFormValues("contractAddress", e.target.value);
+          }}
+          type="text"
+          placeholder="Contract address"
+        />
+
+        <Button label="Submit" className="is-fullwidth" />
+      </InternalPage>
     </Page>
   );
 };
